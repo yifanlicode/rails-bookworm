d9b9cc30484460bc7852177bec064fbbd4a87d2d
yifanli <yifanlicode@gmail.com>
2023-07-19 17:11:20 -0500

    Add header and footer
    add header and footer in the views/home, and render in the layouts/application.html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,16 @@ class HomeController < ApplicationController
   def index
   end
 
-  def about 
+  def about
     
-  end
+    @about_me = "I'm a Full Stack Web Development student at Red River College, 
+    currently in my third term.  
+    Right now,,I'm diving into frameworks like Ruby on Rails.
+    I really enjoy coding and. leaning new things, 
+    building something from scratch is  incredibly fun and cool. 
+    I hope someday I can develop applications to solve real-world problems."
+   
 
+  end
 
 end
