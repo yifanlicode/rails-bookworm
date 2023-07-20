31cbe018ec55882df03a6753b5156eb3293b700f
yifanli <yifanlicode@gmail.com>
2023-07-19 22:57:23 -0500

    Add seeds.rb
    get data from CSV

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: "home#index"
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
+  resources :products, only: [:index, :show]
+
+
 
   get "/about", to: "home#about"
   get "/contact", to: "home#contact"
+
+  root to: "home#index"
+
+  ActiveAdmin.routes(self)
+
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+require 'csv'
+
+Product.destroy_all
+Category.destroy_all
+
+# Create a default email and password for ActiveAdmin
+# AdminUser.create!(email: 'yifan@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# Create 10 categories about Books
+10.times do
+  Category.create(name: Faker::Book.genre)
+end 
+
+# 读取CSV文件
+csv_file = Rails.root.join('db/books.csv')
+
+# 使用CSV.foreach并设置引号为nil
+CSV.foreach(csv_file, headers: true, col_sep: ',', quote_char: nil) do |row|
+  title = row['title']
+  authors = row['authors']
+  published_year = row['published_year'].to_s.strip
+  description = row['description']
+  isbn = row['isbn10']
+  isbn13 = row['isbn13']
+  average_rating = row['average_rating']
+  thumbnail = row['thumbnail']
+  ratings_count = row['ratings_count']
+  num_pages = row['num_pages']
+
+  next if authors.blank? || isbn.blank? || isbn13.blank? || title.blank? || description.blank? || average_rating.blank?  || published_year.blank? || thumbnail.blank? || ratings_count.blank? || num_pages.blank?
+
+  product_data = {
+    title: title,
+    authors: authors,
+    description: description,
+    isbn: isbn,
+    isbn13: isbn13,
+    average_rating: average_rating,
+    published_year: published_year,
+    thumbnail: thumbnail,
+    ratings_count: ratings_count,
+    num_pages: num_pages,
+    price: row['price'] || 20,
+    stock: row['stock'] || 100
+  }
+
+  product = Product.create!(product_data)
+
+
+  # add categories to product randomly
+  categories = Category.order('RANDOM()').limit(2)
+  product.categories << categories
+
+end
+
