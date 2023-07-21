92c235211254fbcf206ed46a6550b30c8a32cc30
yifanli <yifanlicode@gmail.com>
2023-07-20 22:09:00 -0500

    Set AWS S3
    Configuration

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
 
   has_and_belongs_to_many :categories
+  # has_one_attached :image
+
 
   # Validations
   validates :title, presence: true
@@ -13,17 +15,6 @@ class Product < ApplicationRecord
   scope :new_arrivals, -> { where(status: 'new_arrivals') }
   scope :out_of_stock, -> { where(status: 'out_of_stock') }
 
-
-  # has_many_attached :images do |attachable|
-  #   attachable.variant :thumb, resize: "100x100"
-  #   attachable.variant :medium, resize: "300x300"
-  # end
-
-  has_one_attached :image do |attachable|
-    attachable.variant :thumb, resize: "100x100"
-    attachable.variant :medium, resize: "300x300"
-  end
-  
 
   def self.ransackable_attributes(auth_object = nil)
     ["authors", "average_rating", "created_at", "description", "id", "isbn", "isbn13", "num_pages", "price", "published_year", "ratings_count", "stock", "thumbnail", "title", "updated_at"]
