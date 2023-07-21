5d134ec1c9c78e932116bc77a869afab5fde9dd9
yifanli <yifanlicode@gmail.com>
2023-07-20 20:19:37 -0500

    Update Category table
    get real data(not faker) from the csv file , almost 99 rows. need to be reconsidered

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,15 @@ Category.destroy_all
 # AdminUser.create!(email: 'yifan@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
 # Create 10 categories about Books
-10.times do
-  Category.create(name: Faker::Book.genre)
-end 
+# 10.times do
+#   Category.create(name: Faker::Book.genre)
+# end 
 
-# 读取CSV文件
 csv_file = Rails.root.join('db/books.csv')
 
-# 使用CSV.foreach并设置引号为nil
+# Create a hash to store the category name and their corresponding id
+category_map = {}
+
 counter = 0
 CSV.foreach(csv_file, headers: true, col_sep: ',', quote_char: nil) do |row|
   title = row['title']
@@ -28,8 +29,7 @@ CSV.foreach(csv_file, headers: true, col_sep: ',', quote_char: nil) do |row|
   ratings_count = row['ratings_count']
   num_pages = row['num_pages']
 
-  next if authors.blank? || isbn.blank? || isbn13.blank? || title.blank? || description.blank? || average_rating.blank?  || published_year.blank? || thumbnail.blank? || ratings_count.blank? || num_pages.blank?
-
+  next if authors.blank? || isbn.blank? || isbn13.blank? || title.blank? || description.blank? || thumbnail.blank?
   next if published_year.to_i.zero? || average_rating.to_i.zero? || ratings_count.to_i.zero? || num_pages.to_i.zero?
   product_data = {
     title: title,
@@ -47,14 +47,20 @@ CSV.foreach(csv_file, headers: true, col_sep: ',', quote_char: nil) do |row|
   }
 
   product = Product.create!(product_data)
-
-
-  # add categories to product randomly
-  categories = Category.order('RANDOM()').limit(2)
-  product.categories << categories
-
   counter += 1
   break if counter >= 1000
+
+  # Extract the categories from the row
+  categories = row['categories'].split(',')
+
+  next if categories.blank?
+
+  # Create the categories in the database
+  categories.each do |category_name|
+    category_name.strip!
+    category = category_map[category_name] ||= Category.find_or_create_by(name: category_name)
+    product.categories << category
+  end
 
 end
 
