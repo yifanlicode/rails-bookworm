97e957cfd613a4a9ef069f19dff2112e80e9becb
yifanli <yifanlicode@gmail.com>
2023-08-10 13:20:02 -0500

    Feat : admin can manage orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,12 @@ class Order < ApplicationRecord
   has_many :order_items
 
   validates :total_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["address_id", "created_at", "id", "status", "total_amount", "updated_at", "user_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["order_items", "user"]
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,4 +6,8 @@ class OrderItem < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "order_id", "price", "product_id", "quantity", "updated_at"]
+  end
+
 end
